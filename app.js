21214c6b6dc04bda413e722f7bc8dac3ce10ed90
wlwl0219 <wlwl0219@gmail.com>
2020-11-11 21:55:19 +0900

    modify server setting

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 8001;
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
+app.use(express.static(__dirname + "/public"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(port, () => {
