9294aa6ac847264985e712f4257941b3c739037d
wlwl0219 <wlwl0219@gmail.com>
2020-11-14 14:17:30 +0900

    modify2 post controller

--- controller/posts/writeedit.js
+++ controller/posts/writeedit.js
@@ -1,17 +1,20 @@
 const { posts } = require("../../models");
 
 module.exports = {
-  put: (req, res) => {
+  put: async (req, res) => {
     // 태그를 어떻게 받아올지 고민!!!!!!!!!!!
     let id = req.params.id;
     const { title, thumbnail, content } = req.body;
 
     if (id) {
-      const postEdit = await posts.update({
-        title:title, 
-        thumbnail:thumbnail, 
-        content: content
-      },{ where: { id: id } });
+      const postEdit = await posts.update(
+        {
+          title: title,
+          thumbnail: thumbnail,
+          content: content,
+        },
+        { where: { id: id } }
+      );
 
       if (postEdit) {
         return res.status(200).json(postEdit);
